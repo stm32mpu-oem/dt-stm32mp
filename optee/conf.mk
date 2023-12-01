58f961d9d6a85cd2c7a3971f063faf1f94e29e32
Christophe Priouzeau <christophe.priouzeau@foss.st.com>
2023-12-01 15:39:29 +0100

    v5.0-stm32mp-r1
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@foss.st.com>

---
+++ optee/conf.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+# MP13 boards
+
+# MP15 boards
+
+# MP25 boards
+flavor_dts_file-257F_EV1_REV_B_OSTL = stm32mp257f-ev1-revB-ca35tdcid-ostl.dts
+flavorlist-MP25-REV-A-B += $(flavor_dts_file-257F_EV1_REV_B_OSTL)
+
+flavor_dts_file-257F_EV1_OSTL = stm32mp257f-ev1-ca35tdcid-ostl.dts
+flavorlist-MP25 += $(flavor_dts_file-257F_EV1_OSTL)
+
+flavor_dts_file-257F_EV1_REV_B_OSTL_M33_EXAMPLES = stm32mp257f-ev1-revB-ca35tdcid-ostl-m33-examples.dts
+flavorlist-MP25-REV-A-B += $(flavor_dts_file-257F_EV1_REV_B_OSTL_M33_EXAMPLES)
+
+flavor_dts_file-257F_EV1_OSTL_M33_EXAMPLES = stm32mp257f-ev1-ca35tdcid-ostl-m33-examples.dts
+flavorlist-MP25 += $(flavor_dts_file-257F_EV1_OSTL_M33_EXAMPLES)
